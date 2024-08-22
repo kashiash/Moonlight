ae38859b17feebb52f2c237db3220f388019ad39
Jacek Kosinski <kashiash@gmail.com>
2024-08-22 11:13:55 +0200

    Initial Commit

---
+++ Moonlight/MoonlightApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonlightApp.swift
+//  Moonlight
+//
+//  Created by Jacek Kosinski U on 22/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoonlightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
